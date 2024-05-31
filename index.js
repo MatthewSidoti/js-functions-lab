9e498e033eba0da233301d1b32cce4eec1cd6d2d
MatthewSidoti <coding.sidoti@gmail.com>
2024-05-30 21:31:16 -0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// function name(){
-// console.log('matthew')
-// }
+function name(){
+console.log('matthew')
+}
+name()
 
 // const maxOfTwoNumbers = (x, y) => {
 //     if (x >= y) {
@@ -22,10 +23,31 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-let x=18
-  if (x <= 17){
-    return console.log("Minor")} 
-else {console.log("Adult");}
+// let x=18
+//   name() if (x <= 17){
+//     return console.log("Minor")} 
+// else {console.log("Adult");}
 
-//Exercise 3
+function isAdult(x){
+    if (x <= 17) {console.log("Minor")}
+        else {console.log("Adult");}
+    
+}
+isAdult(10)
+/*
+Exercise 3: isCharAVowel()
 
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+// const vowel= (a,e,i,o,u)
+// const consonant=(B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z)
+// if (vowel){
+// console.log(isCharAVowel("a");
