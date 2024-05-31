f478cc748c31b82cafeb55c534f527778299289b
MatthewSidoti <coding.sidoti@gmail.com>
2024-05-30 20:41:55 -0400

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+// function name(){
+// console.log('matthew')
+// }
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+  if (isAdult <= 21){
+    return isAdult} 
+else {console.log('Exercise 2 Result:', isAdult(21));}
