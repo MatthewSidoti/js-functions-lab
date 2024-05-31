e28accd03f7aaa674f7dbeda8381f4347a7878d3
MatthewSidoti <coding.sidoti@gmail.com>
2024-05-31 19:35:02 -0400

    update exercise 7

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ Complete the exercise in the space below:
 //     return console.log("Minor")} 
 // else {console.log("Adult");}
 
-function isAdult(x){
-    if (x <= 17) {console.log("Minor")}
+function isAdult(age){
+    if (age <= 17) {console.log("Minor")}
         else {console.log("Adult");}
     
 }
@@ -47,7 +47,82 @@ Example: isCharAVowel('a') should return true.
 Complete the exercise in the space below:
 */
 
-// const vowel= (a,e,i,o,u)
-// const consonant=(B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z)
-// if (vowel){
-// console.log(isCharAVowel("a");
+const vowel= "aeiou"
+function isCharAVowel(letter){
+if (vowel.includes(letter)){
+console.log("true")}
+else{
+    console.log("false")
+}}
+isCharAVowel("b")
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+
+function generateEmail (firstName, lastName) {
+    console.log(firstName + lastName + "@example.com")
+}
+
+
+generateEmail("Matthew", "Sidoti")
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser(name, timeOfDay){
+    console.log("Good" + " " + timeOfDay + "," + " " + name +"!" )
+}
+greetUser("Matthew", "morning")
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(numberOne, numberTwo, numberThree){
+    console.log (Math.max(numberOne, numberTwo, numberThree))
+}
+maxOfThree(17, 4, 9)
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
