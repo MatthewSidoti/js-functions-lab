0510efa703d4a592f8877cc3729c770ed1aee702
MatthewSidoti <coding.sidoti@gmail.com>
2024-05-30 20:55:02 -0400

    completed exercise 2

--- index.js
+++ index.js
@@ -2,17 +2,30 @@
 // console.log('matthew')
 // }
 
-const maxOfTwoNumbers = (x, y) => {
-    if (x >= y) {
-      return x;
-    } else {
-      return y;
-    }
-  }
+// const maxOfTwoNumbers = (x, y) => {
+//     if (x >= y) {
+//       return x;
+//     } else {
+//       return y;
+//     }
+//   }
   
-  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+//   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 
+/*
+Exercise 2: isAdult()
 
-  if (isAdult <= 21){
-    return isAdult} 
-else {console.log('Exercise 2 Result:', isAdult(21));}
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+let x=18
+  if (x <= 17){
+    return console.log("Minor")} 
+else {console.log("Adult");}
+
+//Exercise 3
+
